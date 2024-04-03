7aa32e4818e5755b2835df7b3f147b6110600023
ZubRahman <163024202+ZubRahman@users.noreply.github.com>
2024-04-03 14:26:24 +0100

    Update window (main).py

--- window (main).py
+++ window (main).py
@@ -5,58 +5,12 @@ from EncryptUnecryptData import *
 from LoginAuthenticationSystem import *
 
 
+def plane_tracking_labels(flightDetails,WeatherDetails):
+    flightDetails_label = Label(window, text=scheduled)
+    WeatherDetails_label = Label(window, text=scheduled)
 
-
-#example
-def plane_tracking_labels(flightdate, flightstatus, departure, airport, timezone, terminal, gate, delay, scheduled, estimated, arrivaldestination, timezone2, terminal2, gate2):
-    flight_date = Label(window, text = flightdate)
-    flight_date.pack()
-
-    flightstatus_label = Label(window, text = flightstatus)
-    flightstatus_label.pack()
-
-    departure_label = Label(window, text = departure)
-    departure_label.pack()
-
-    airport_label = Label(window, text = airport)
-    airport_label.pack()
-
-    timezone_label = Label(window, text = timezone)
-    timezone_label.pack()
-
-    terminal_label = Label(window, text = terminal)
-    terminal_label.pack()
-
-    gate_label = Label(window, text = gate)
-    gate_label.pack()
-
-    delay = Label(window, text = delay)
-    delay.pack()
-
-    scheduled_label = Label(window, text = scheduled)
-    scheduled_label.pack()
-
-    estimated_label = Label(window, text = estimated)
-    estimated_label.pack()
-
-    arrival_destination_label = Label(window, text = arrivaldestination)
-    arrival_destination_label.pack()
-
-    #arrival
-
-    arrival_ = Label(window, text= "arrival :")
-    arrival_.pack()
-
-    timezone2_label = Label(window, text = timezone2)
-    timezone2_label.pack()
-
-    terminal2_label = Label(window, text=terminal2)
-    terminal2_label.pack()
-
-    gate2_label = Label(window, text=gate2)
-    gate2_label.pack()
-
-
+    for pack_ in (flightDetails_label,WeatherDetails_label): 
+        pack_.pack()
 
 def weather_labels(country):
     from weather import weather
@@ -86,20 +40,13 @@ def weather_labels(country):
 def encryptdata(data):
     hashdata(data)
 
-
-
-
-# flightdate, flightstatus, departure, airport, timezone, terminal, gate, delay, scheduled, estimated, arrivaldestination, airport2, timezone2, terminal2, gate2
-#example
-
 from LoginAuthenticationSystem import window,login
 login_window = window()
 
 window = tk.Tk()
 window.geometry("1280x720")
 
-plane_tracking_labels("2024-04-01", "scheduled", "departure:", "airport: Heathrow", "timezone: Europe/London", "terminal: 3", "gate : 7", "delay : 76",
-       "scheduled: 2024-04-01T14:20:00+00:00", "estimated: 2024-04-01T14:20:00+00:00", "airport: Dubai", "timezone: Asia/Dubai", "terminal: 3", "gate: None")
+plane_tracking_labels()
 
 weather_labels("England")
 
